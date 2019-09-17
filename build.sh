27591f12eadbf83c9077d217c58402b8019cfafa
Kaloyan Arsov <kaloyan.arsov@gmail.com>
2019-09-17 19:30:55 +0300

    latest dev

---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t dontar/snapcap-dev:node-lts-alpine ./node \
+    && docker push dontar/snapcap-dev:node-lts-alpine
+docker build -t dontar/snapcap-dev:php-7-alpine ./php \
+    && docker push dontar/snapcap-dev:php-7-alpine
