b474f0f6512b2693f482d810ee9675c84315840b
Kaloyan Arsov <kaloyan.arsov@gmail.com>
2019-10-06 21:19:30 -0400

    make build executable

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 docker build -t dontar/snapcap-dev:node-lts-alpine ./node \
     && docker push dontar/snapcap-dev:node-lts-alpine
 docker build -t dontar/snapcap-dev:php-7-alpine ./php \
