94d5d354e91d15cd41521e79baa3c970037fcc7f
Kaloyan Arsov <kaloyan.arsov@gmail.com>
2019-10-11 13:54:09 -0400

    Build alpine

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+# Build debian
 docker build -t dontar/snapcap-dev:node-lts ./node \
     && docker push dontar/snapcap-dev:node-lts
 docker build -t dontar/snapcap-dev:php-7 ./php \
     && docker push dontar/snapcap-dev:php-7
+
+# Build alpine
+docker build -t dontar/snapcap-dev:node-lts-alpine ./node-alpine \
+    && docker push dontar/snapcap-dev:node-lts-alpine
+docker build -t dontar/snapcap-dev:php-7-alpine ./php-alpine \
+    && docker push dontar/snapcap-dev:php-7-alpine
